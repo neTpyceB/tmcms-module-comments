0a7c8583e8df6c035b55aea7131dde28178606e1
Vadims Petrusevs <pvman@inbox.ru>
2015-02-02 16:59:54 +0200

    Objects

---
+++ src/neTpyceB/TMCms/Modules/Comments/Object/Comment.php
@@ -0,0 +1,18 @@
+<?php
+namespace neTpyceB\TMCms\Modules\Comments\Object;
+
+use neTpyceB\TMCms\Modules\CommonObject;
+use neTpyceB\TMCms\Strings\Converter;
+
+/**
+ * Class Product
+ * @method getText() string
+ */
+class Comment extends CommonObject {
+    protected $db_table = 'm_comments';
+
+    protected $client_id = 0;
+    protected $item_type = '';
+    protected $item_id = 0;
+    protected $text = '';
+}
